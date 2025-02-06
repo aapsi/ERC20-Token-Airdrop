98cedafc39701bb14f09dc96262af93c81fa2cde
aapsi <aapsikhaira98@gmail.com>
2025-02-06 08:45:14 +0530

    Update GenerateTree.js to read values from a CSV file instead of hardcoded array

--- scripts/GenerateTree.js
+++ scripts/GenerateTree.js
@@ -2,10 +2,19 @@ import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 import fs from "fs";
 
 // (1)
-const values = [
-  ["0x1111111111111111111111111111111111111111", "5000000000000000000"],
-  ["0x2222222222222222222222222222222222222222", "2500000000000000000"]
-];
+// const values = [
+//   ["0x1111111111111111111111111111111111111111", "5000000000000000000"],
+//   ["0x2222222222222222222222222222222222222222", "2500000000000000000"]
+// ];
+
+// read values from csv file
+
+const csv = fs.readFileSync("values.csv", "utf8");
+const values = csv.split("\n").map(line => {
+  const [address, amount] = line.split(",");
+  return [address, amount];
+});
+
 
 // (2)
 const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
